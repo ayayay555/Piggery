9ce8be2d5197010b0c7626380323cbd24240f134
ayayay555 <karljhunzgonzales@gmail.com>
2026-03-28 01:52:55 -0700

    ⏪ YOLO: Removed custom domain and restored free GitHub Pages URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://caduhaanpiggery.com',
+  site: 'https://ayayay555.github.io',
+  base: '/Piggery',
   output: 'static',
 });
