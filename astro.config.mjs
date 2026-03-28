889d1325df14b728a37697f9c0ba89725da30143
ayayay555 <karljhunzgonzales@gmail.com>
2026-03-28 01:51:16 -0700

    🌐 YOLO: Configured custom domain for caduhaanpiggery.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ayayay555.github.io',
-  base: '/Piggery',
+  site: 'http://caduhaanpiggery.com',
   output: 'static',
 });
